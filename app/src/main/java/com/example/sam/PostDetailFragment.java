d813b6c4074a6882ccd27c90c6c38daa56e0738b
junghwa1 <duawhk0508@naver.com>
2024-07-28 23:40:15 +0900

    sencond

--- app/src/main/java/com/example/sam/PostDetailFragment.java
+++ app/src/main/java/com/example/sam/PostDetailFragment.java
@@ -4,9 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.ListView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -14,31 +12,23 @@ import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 
-public class HomeFragment extends Fragment {
+public class PostDetailFragment extends Fragment {
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_post_detail, container, false);
 
-        // 더보기 버튼 설정
-        Button buttonMore = view.findViewById(R.id.more_post);
-        buttonMore.setOnClickListener(new View.OnClickListener() {
+        // "목록으로" 버튼 설정
+        Button buttonApply = view.findViewById(R.id.apply);
+        buttonApply.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // 네비게이션을 통해 PostListFragment로 전환
                 NavController navController = Navigation.findNavController(v);
-                navController.navigate(R.id.action_homeFragment_to_postListFragment);
+                navController.navigate(R.id.action_postDetailFragment_to_postListFragment);
             }
         });
-
-        // ListView 설정
-        ListView listView = view.findViewById(R.id.listView);
-        String[] items = {"Item 1", "Item 2", "Item 3"};
-
-        // 어댑터 설정
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(getContext(), R.layout.post_more, R.id.textView, items);
-        listView.setAdapter(adapter);
 
         return view;
     }
--- app/src/main/java/com/example/sam/PostListFragment.java
+++ app/src/main/java/com/example/sam/PostListFragment.java
@@ -5,32 +5,18 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.ListView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
 
-public class HomeFragment extends Fragment {
+public class PostListFragment extends Fragment {
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_home, container, false);
-
-        // 더보기 버튼 설정
-        Button buttonMore = view.findViewById(R.id.more_post);
-        buttonMore.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // 네비게이션을 통해 PostListFragment로 전환
-                NavController navController = Navigation.findNavController(v);
-                navController.navigate(R.id.action_homeFragment_to_postListFragment);
-            }
-        });
+        View view = inflater.inflate(R.layout.fragment_post_list, container, false);
 
         // ListView 설정
         ListView listView = view.findViewById(R.id.listView);
